957e98cb76203d13454e18e67dabc2cbde6c75e0
John Weir <jweir@ise.com>
2015-10-07 11:16:57 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
-
+#
+# This checks to see if there is an existing nessus
+# install in the data container.  If there is not a 
+# directory named /opt/nessus-persist/nessus/var
+# (thus indicating the data container is new as well)
+# the fresh install of Nessus on the newly created
+# container is copied to the data container and
+# the original install location is deleted.  Nessus
+# is now solely installed on the data container
+#
+# The second part of the script is executed, where 
+# the data container directory is soft linked to the
+# standard install location on the new container.
+# 
+# 
 if [ ! -f /opt/nessus-persist/nessus/var ]; then
     /bin/cp -a /opt/nessus /opt/nessus-persist
     /bin/rm -rf /opt/nessus
