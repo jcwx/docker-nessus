d72e168cfead126455a911b14a054fd08701d67f
John Weir <jweir@ise.com>
2015-10-15 12:47:08 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,23 +1,24 @@
 #!/bin/sh
-#
-# This checks to see if there is an existing nessus
-# install in the data container.  If there is not a 
-# directory named /opt/nessus-persist/nessus/var
-# (thus indicating the data container is new as well)
-# the fresh install of Nessus on the newly created
-# container is copied to the data container and
-# the original install location is deleted.  Nessus
-# is now solely installed on the data container
-#
-# The second part of the script is executed, where 
-# the data container directory is soft linked to the
-# standard install location on the new container.
-# 
-# 
-if [ ! -f /opt/nessus-persist/nessus/var ]; then
-    /bin/cp -a /opt/nessus /opt/nessus-persist
+
+# check if previous install is on persistent
+# drive, if not, copy new install to 
+# persistent drive
+if [ ! -d /opt/nessus-persist/var ]; then
+	if [ ! -d /opt/nessus-persist ]; then
+		/bin/mkdir /opt/nessus-persist
+	fi
+    /bin/mv /opt/nessus/* /opt/nessus-persist/
+fi
+
+# cp backup data from previous install into new
+if [ -d /tmp/opt ]; then
+    /bin/cp -rf /tmp/opt/nessus-persist/* /opt/nessus-persist/
+fi
+
+# rm nessus directory if previous install exists
+if [ -d /opt/nessus ]; then
     /bin/rm -rf /opt/nessus
 fi
 
-/bin/ln -s /opt/nessus-persist/nessus /opt/nessus
-/opt/nessus/sbin/nessusd
+/bin/ln -s /opt/nessus-persist /opt/nessus
+/opt/nessus/sbin/nessus-service -D
